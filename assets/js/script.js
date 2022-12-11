888005b4d6da99afc1d0cc93e53efcc574804d8b
angelajm6 <angelajaume.m@gmail.com>
2022-12-11 13:08:27 -0500

    fixed bug in line 34 Js

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,13 +25,13 @@ const months = [
 ];
 
 function initCalendar() {
-  //To get prev month days and current month all days and remaining month days
+//To get prev month days and current month all days and remaining month days
   const firstDay = new Date(year,month,1);
   const lastDay = new Date(year,month + 1,0);
   const prevLastDay = new Date(year,month,0);
     const prevDays = prevLastDay.getDate();
     const lastDate = lastDay.getDate();
-    const day = firstday.getDate();
+    const day = firstDay.getDay();
     const nextDays = 7 - lastDay.getDay() - 1;
 
 //update date on top of the calendar
@@ -42,13 +42,27 @@ function initCalendar() {
 
 //prev month days
   for (let x = day; x >0; x--) {
-    days = '<div class= "day prev-date" >${prevDays - x + 1}</div>';
+    days += '<div class= "day prev-date" >${prevDays - x + 1}</div>';
     }
+    //current month days
+    for(let i = 1; i < lastDate; i++) {
+      //if day is today add class today
+      if (
+        i == new Date().getDate() &&
+        year == new Date().getFullYear() &&
+        month == new Date().getMonth()
+      )
+      days += '<div class= "day today" >${i}</div>';
+      } 
+
+    }
+
+    daysContainer.innerHTML = days;
+
   }
 
 
-
-initCalendar();
+  initCalendar();
 
 
 
