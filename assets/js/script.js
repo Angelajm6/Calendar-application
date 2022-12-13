79b3f5c1da7ca353960b800ec98c97242160734d
angelajm6 <angelajaume.m@gmail.com>
2022-12-13 02:32:36 -0500

    Variable to show current date in header of webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,12 +18,11 @@ $(function () {
   let time = new Date().toLocaleTimeString("en");
   console.log(`Current Time is: ${time}`)
 
-  
-//let today = new Date();
-//let day = today.getDay();
-//let dayList = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-//console.log("Today is : " + dayList[day] + "."); //display the current day
 
+  //show current date in the header of the page
+  var dt = new Date();
+  document.getElementById("currentDay").innerHTML = dt.toLocaleString();
+  
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -34,8 +33,7 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+ 
 });
 
 
