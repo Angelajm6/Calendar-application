a63d244370e9ca9ee2a704105b4d21f1b9554b54
angelajm6 <angelajaume.m@gmail.com>
2022-12-13 21:45:46 -0500

    multiple commits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
 $(function () {
   
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage.
-
+  // listener for click events on the save button. 
   $(".saveBtn").click(function(){
     let descriptionId = `#description-${this.dataset.hour}`
     let task = $(descriptionId).val() 
@@ -34,13 +31,5 @@ $(function () {
     document.getElementById("description").value = myContent;
   }
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  
 });
-
-
-//2. info change based of TimeRanges
-//3. update page with what's in local storage
